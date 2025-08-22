331589140ca3ff2bc93dd8dfbf0cc244f84bb222
Levar Norwood <levar.norwood@outlook.com>
2025-08-21 22:30:44 -0400

    completed split image & link function, next is further testing

--- src/split_textnode.py
+++ src/split_textnode.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from extract_markdown import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     valid_delimiters = ["**", "_", "`"]
@@ -22,5 +23,75 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     text_flag = True
         else:
             raise ValueError(f"Invalid Markdown syntax: '{delimiter}' matching closing delimiter not found,")
+
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        original_text = node.text
+        img_ext = extract_markdown_images(original_text)
+        if len(img_ext) == 0:
+            new_nodes.append(node)
+            continue
+        for image in img_ext:
+
+            image_alt = image[0]
+            image_link = image[1]
+
+            sections = original_text.split(f"![{image_alt}]({image_link})", 1)
+
+            before_img = sections[0]
+            after_img = sections[1]
+
+            if len(before_img) != 0:
+                new_nodes.append(TextNode(before_img, TextType.TEXT))
+                new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+                original_text = after_img
+            else:
+                new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+                original_text = after_img
+        if len(original_text) != 0:
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        original_text = node.text
+        link_ext = extract_markdown_links(original_text)
+        if len(link_ext) == 0:
+            new_nodes.append(node)
+            continue
+        for link in link_ext:
+
+            link_text = link[0]
+            link_url = link[1]
+
+            sections = original_text.split(f"[{link_text}]({link_url})", 1)
+
+            before_link = sections[0]
+            after_link = sections[1]
+
+            if len(before_link) != 0:
+                new_nodes.append(TextNode(before_link, TextType.TEXT))
+                new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+                original_text = after_link
+            else:
+                new_nodes.append(TextNode(link_text, TextType.IMAGE, link_url))
+                original_text = after_link
+        if len(original_text) != 0:
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
 
     return new_nodes
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from split_textnode import split_nodes_delimiter
+from split_textnode import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 
 class TestTextNode(unittest.TestCase):
@@ -64,6 +64,24 @@ class TestTextNode(unittest.TestCase):
         with self.assertRaises(ValueError):
             node = TextNode("This is text with a ++Random text word++", TextType.TEXT)
             split_nodes_delimiter([node], "++", TextType.ITALIC)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
 
 if __name__ == "__main__":
     unittest.main()
