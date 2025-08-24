135dacb487076141b40cfacb3d183e0879b6c6cf
Levar Norwood <levar.norwood@outlook.com>
2025-08-23 23:56:43 -0400

    finished block_markdown and working on blocktype

---
+++ src/blocktype.py
@@ -0,0 +1,35 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(block):
+
+    headings = re.findall(r'^[#]{1,6}\s.+', block)
+    code_blocks = re.findall(r'(?s)([`]{3}).*?([`]{3})', block)
+    quote_blocks = re.findall(r'(?m)(^>)(.*?)$', block)
+    unordered_lists = re.findall(r'(?m)(^-)(.*?)$', block)
+    ordered_lists = re.findall(r'(?m)(^\d+.)(\s.*?)$', block)
+
+    if headings:
+        return BlockType.HEADING
+    elif code_blocks:
+        return BlockType.CODE
+    elif quote_blocks:
+        return BlockType.QUOTE
+    elif unordered_lists:
+        return BlockType.UNORDERED_LIST
+    elif ordered_lists:
+        #Write logic to determine if ordered example:
+            # 1. Item 1
+            # 2. Item 2
+            # 3. Item 3
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
